8172510e6b9f177a425306b0a1644fccf2e633a1
Wiam Sahli <78504532+WiamSAHLI@users.noreply.github.com>
2025-11-19 00:02:36 +0100

    Delete einstellige-quersumme zahl.rkt

--- einstellige-quersumme zahl.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-(define(einstellige-quersumme zahl)
-  (define(iter zahl sum)
-    (if(= zahl 0)
-       sum
-       (iter (quotient zahl 10)(+ sum (remainder zahl 10)))))
-    (iter zahl 0))
-(einstellige-quersumme 123)
