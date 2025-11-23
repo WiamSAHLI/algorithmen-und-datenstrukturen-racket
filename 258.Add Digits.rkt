c56d30b546e232420220e2b20188375f8b34ab8e
Wiam Sahli <78504532+WiamSAHLI@users.noreply.github.com>
2025-11-23 10:06:16 +0100

    Update 258.Add Digits.rkt

--- 258.Add Digits.rkt
+++ 258.Add Digits.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 ;methode 1:
+
 (define(add-digits num)
   (define(iter n add)
   (if(= n 0)
@@ -9,13 +10,16 @@
      (iter(quotient n 10)
           (+ add(remainder n 10)))))
   (iter (abs num) 0))
+
 (add-digits -38)
+(add-digits 38)
+
 ;methode 2:
+
 (define ( adddigits num)
   
   (if(<= (abs num) 9)
      (abs num)
      (adddigits(+(quotient num 10)(remainder num 10)))))
-
 
 (adddigits -38)
